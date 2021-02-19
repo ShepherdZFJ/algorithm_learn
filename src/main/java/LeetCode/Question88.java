ed4373bd3ad21919cec5e155468f5d52bbf73405
shepherd <zhengfj@isyscore.com>
2021-02-19 19:06:52 +0800

    add：添加排序

---
+++ src/main/java/LeetCode/Question88.java
@@ -0,0 +1,54 @@
+package LeetCode;
+
+/**
+ * @author fjZheng
+ * @version 1.0
+ * @date 2021/2/19 16:20
+ */
+
+/**
+ * 题目描述：给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+ *
+ * 示例 1：
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ *
+ * 示例 2：
+ * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+ * 输出：[1]
+ */
+public class Question88 {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m=3, n=3;
+        merge(nums1, m, nums2, n);
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i]+" ");
+        }
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] result = new int[m+n];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i<m && j<n) {
+            if(nums1[i] <= nums2[j]) {
+                result[k++] = nums1[i++];
+            } else {
+                result[k++] = nums2[j++];
+            }
+        }
+        while (i<m) {
+            result[k++] = nums1[i++];
+        }
+        while (j<m) {
+            result[k++] = nums2[j++];
+        }
+        System.arraycopy(result, 0, nums1, 0, result.length);
+
+
+    }
+}
+
--- src/main/java/pat/SortExample.java
+++ src/main/java/pat/SortExample.java
@@ -13,7 +13,9 @@ public class SortExample {
     public static void main(String[] args) {
         int[] nums = {35,18,16,72,24,65,12,88,46,28,55};
        // selectSort(nums);
-        insertSort(nums);
+       // insertSort(nums);
+       // mergeSort(nums);
+        quickSort(nums, 0, nums.length -1);
         for(int i = 0; i <nums.length; i++) {
             System.out.print(nums[i]+" ");
         }
@@ -63,4 +65,92 @@ public class SortExample {
             nums[j] = temp;
         }
     }
+
+
+    /**
+     * 归并排序：2路归并排序的原理是将序列两两分组，即分成n/2个组，组内单独排序，然后再将这些组再两两归并，生成n/4个组，组内在单独
+     * 排序，以此类推，知道剩下一个组为止，即最终是一个有序的数组。归并排序的时间复杂度O(nlogn)
+     * @param nums
+     * @param left
+     * @param right
+     */
+    // 递归实现归并排序算法
+    static void mergeSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid+1, right);
+            merge(nums, left, mid, mid+1, right);
+        }
+    }
+    // 合并两个有序数组
+    static void merge(int[] nums, int left1, int right1, int left2, int right2) {
+        int i = left1;
+        int j = left2;
+        int[] result = new int[nums.length];
+        int k = 0;
+        while (i <= right1 && j <=right2) {
+            if (nums[i] <= nums[j]) {
+                result[k++] = nums[i++];
+            } else {
+                result[k++] = nums[j++];
+            }
+        }
+        while(i <= right1) {
+            result[k++] = nums[i++];
+        }
+        while(j <= right2) {
+            result[k++] = nums[j++];
+        }
+        for (int x = 0; x < k; x++) {
+            nums[left1+x] = result[x];
+        }
+    }
+
+    //2.非递归实现算法(待研究)
+    static void mergeSort(int[] nums) {
+        int n = nums.length;
+        for (int step = 2; step / 2 <= n; step *=2 ) {
+            for (int i = 1; i <= n; i += step) {
+                int mid = i + step / 2 - 1;
+                if (mid + 1 <= n) {
+                    merge(nums, i, mid, mid+1, Math.min(i + step - 1, n));
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 快速排序：快速排序是排序算法中平均时间复杂度为O(nlogn)的一种算法，核心思想：对于一个序列nums[0], nums[1],.....nums[n-1],
+     * 调整序列中元素的位置，使得nums[0]的左侧所有元素都小于nums[0], 右侧所有元素都大于nums[0]，这样nums[0]就到了最终位置了，接
+     * 下来只需要使用递归对nums[0]左右两侧的元素进行上面的操作即可，最终形成的序列即是一个有序的数组
+     * @param nums
+     * @param left
+     * @param right
+     */
+    static void quickSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int pos = partition(nums, left, right);
+            quickSort(nums, left, pos - 1);
+            quickSort(nums, pos + 1, right);
+        }
+    }
+
+    static int partition(int[] nums, int left, int right) {
+        int temp = nums[left];
+        while (left < right) {
+            while (left < right && nums[right] > temp) {
+                right--;
+            }
+            nums[left] = nums[right];
+            while (left < right && nums[left] <= temp) {
+                left++;
+            }
+            nums[right] = nums[left];
+        }
+        nums[left] = temp;
+        return left;
+    }
+
 }
