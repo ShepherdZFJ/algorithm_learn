def6a5b39ad19c456357afef57a6e71853fe4b31
fjzheng <1059959730@qq.com>
2021-11-11 20:15:21 +0800

    update：解决超时报错问题，还可以再优化

--- src/main/java/LeetCode/slidingwindow/Question76.java
+++ src/main/java/LeetCode/slidingwindow/Question76.java
@@ -15,11 +15,10 @@ import java.util.*;
  * 如果 s 中存在这样的子串，我们保证它是唯一的答案。
  *
  * 示例 1：
- *
  * 输入：s = "ADOBECODEBANC", t = "ABC"
  * 输出："BANC"
- * 示例 2：
  *
+ * 示例 2：
  * 输入：s = "a", t = "a"
  * 输出："a"
  * 示例 3:
@@ -36,29 +35,62 @@ import java.util.*;
 public class Question76 {
     public static void main(String[] args) {
         Question76 q = new Question76();
-        String s = "abc";
-        String t = "bb";
+        String s = "a";
+        String t = "a";
         String res = q.minWindow(s, t);
         System.out.println(res);
 
     }
 
+    /**
+     * 滑动窗口算法详见：
+     * 滑动窗口思想：滑动窗口算法的思路是这样：https://github.com/labuladong/fucking-algorithm/blob/master/%E7%AE%97%E6%B3%95%E6%80%9D%E7%BB%B4%E7%B3%BB%E5%88%97/%E6%BB%91%E5%8A%A8%E7%AA%97%E5%8F%A3%E6%8A%80%E5%B7%A7.md
+     *
+     * 1、我们在字符串 S 中使用双指针中的左右指针技巧，初始化 left = right = 0，把索引闭区间 [left, right] 称为一个「窗口」。
+     *
+     * 2、我们先不断地增加 right 指针扩大窗口 [left, right]，直到窗口中的字符串符合要求（包含了 T 中的所有字符）。
+     *
+     * 3、此时，我们停止增加 right，转而不断增加 left 指针缩小窗口 [left, right]，直到窗口中的字符串不再符合要求（不包含 T 中的所有字符了）。同时，每次增加 left，我们都要更新一轮结果。
+     *
+     * 4、重复第 2 和第 3 步，直到 right 到达字符串 S 的尽头。
+     * @param s
+     * @param t
+     * @return
+     */
 
     public String minWindow(String s, String t) {
         int left = 0, right = 0;
-        String result = "";
+        //获取包含字符的数量map
         Map<Character, Integer> needMap = charCount(t);
+        Map<Character, Integer> current = new HashMap<>();
         List<Character> window = new ArrayList<>();
+        int ansL = 0, ansR=0;
+        int len = Integer.MAX_VALUE;
         while(right < s.length()) {
-            window.add(s.charAt(right));
-            right++;
-            while (contain(window, needMap)) {
-                result = minLen(window, result);
+            char c = s.charAt(right);
+            //往滑动窗口添加字符  这个window不需要客观存在，这是优化点
+            window.add(c);
+            //记录滑动窗口的字符数量信息
+            current.put(c, current.getOrDefault(c, 0) + 1);
+            // 判断滑动窗口子串是否包含了要求的字符
+            while (check(needMap, current)) {
+                // 获取最小子串信息，记录开始位置，和结束位置
+                if (right + 1 - left < len) {
+                    len = right - left + 1;
+                    ansL = left;
+                    ansR = right  + 1;
+                }
+                //先对滑动窗口的第一个字符统计减1，方面后面删除该字符，left++
+                if (current.containsKey(window.get(0))) {
+                    current.put(window.get(0), current.get(window.get(0)) - 1);
+                }
+                // 删除滑动窗口第一个字符，即left右移
                 window.remove(0);
-                left++;
+               left++;
             }
+            right++;
         }
-        return result;
+        return s.substring(ansL, ansR);
 
 
     }
@@ -67,46 +99,28 @@ public class Question76 {
         Map<Character ,Integer> map = new HashMap<>();
         for (int i = 0; i < t.length(); i++) {
             char c = t.charAt(i);
-            if (map.get(c) != null) {
-                map.put(c, map.get(c)+1);
-            } else {
-                map.put(c, 1);
-            }
+           map.put(c, map.getOrDefault(c, 0) + 1);
         }
         return map;
     }
 
-    public Boolean contain(List<Character> window, Map<Character, Integer> needMap) {
-        String str = listToString(window);
-        Map<Character ,Integer> current = charCount(str);
+    public Boolean check(Map<Character, Integer> needMap, Map<Character, Integer> current) {
+        if (needMap.size() > current.size()) {
+            return false;
+        }
         for (Map.Entry<Character, Integer> entry : needMap.entrySet()) {
             Character key = entry.getKey();
             Integer value = entry.getValue();
-            if (current.get(key) == null) {
-                return false;
-            }
-            if (current.get(key) < value) {
+            if (current.getOrDefault(key, 0) < value) {
                 return false;
             }
         }
         return true;
     }
 
-    public String listToString(List<Character> window) {
-        StringBuilder s = new StringBuilder();
-        window.forEach(c -> {
-            s.append(c);
-        });
-        return s.toString();
-    }
 
-    public String minLen(List<Character> window ,String result) {
-        String str = listToString(window);
-        if (Objects.equals(result, "")) {
-            return str;
-        }
-        return str.length() <= result.length() ? str : result;
-    }
+
+
 
 
 }
