fbd28abb54caa1f94d8a424f146b95c56d873c6a
fjzheng <1059959730@qq.com>
2021-12-10 11:08:30 +0800

    update：完善单调栈相关题目描述和题解思想

--- src/main/java/LeetCode/stack/Question739.java
+++ src/main/java/LeetCode/stack/Question739.java
@@ -9,6 +9,23 @@ import java.util.Stack;
  * @version 1.0
  * @date 2021/12/8 18:29
  */
+
+/**
+ * 题目描述：请根据每日 气温 列表 temperatures ，请计算在每一天需要等几天才会有更高的温度。如果气温在这之后都不会升高，
+ * 请在该位置用 0 来代替。
+ *
+ * 示例 1:
+ * 输入: temperatures = [73,74,75,71,69,72,76,73]
+ * 输出: [1,1,4,2,1,1,0,0]
+ *
+ * 示例 2:
+ * 输入: temperatures = [30,40,50,60]
+ * 输出: [1,1,1,0]
+ *
+ * 示例 3:
+ * 输入: temperatures = [30,60,90]
+ * 输出: [1,1,0]
+ */
 public class Question739 {
     public static void main(String[] args) {
         int[] temperatures = {200,34,225,28,222,128,53,50,247};
@@ -18,6 +35,12 @@ public class Question739 {
     }
 
 
+    /**
+     * 解题思路：单调栈算法思想，这个问题本质上也是找 Next Greater Number，只不过现在不是问你 Next Greater Number 是多少，
+     * 而是问你当前距离 Next Greater Number 的距离而已。
+     * @param temperatures
+     * @return
+     */
     public int[] dailyTemperatures(int[] temperatures) {
         Stack<Integer> stack = new Stack<>();
         int[] res = new int[temperatures.length];
