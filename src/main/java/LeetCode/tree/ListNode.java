37ec439e9e540b6e27cb982c4d9d39afe5ba2171
fjzheng <1059959730@qq.com>
2022-02-09 18:51:31 +0800

    add：添加树的相关问题题解

---
+++ src/main/java/LeetCode/tree/ListNode.java
@@ -0,0 +1,14 @@
+package LeetCode.tree;
+
+/**
+ * @author fjzheng
+ * @version 1.0
+ * @date 2021/12/8 11:15
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
