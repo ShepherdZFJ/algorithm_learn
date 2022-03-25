3f593e460e03aa641304d6b5db982fd938c59744
fjzheng <1059959730@qq.com>
2022-03-25 18:42:59 +0800

    update：添加相关树问题题解

--- src/main/java/LeetCode/tree/Question226.java
+++ src/main/java/LeetCode/tree/Question226.java
@@ -10,6 +10,25 @@ import java.util.List;
  * @version 1.0
  * @date 2021/2/22 18:29
  */
+
+/**
+ * 题目描述：给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ * 示例 1：
+ * 输入：root = [4,2,7,1,3,6,9]
+ * 输出：[4,7,2,9,6,3,1]
+ *
+ * 示例 2：
+ * 输入：root = [2,1,3]
+ * 输出：[2,3,1]
+ *
+ * 示例 3：
+ * 输入：root = []
+ * 输出：[]
+ *
+ * 提示：
+ * 树中节点数目范围在 [0, 100] 内
+ * -100 <= Node.val <= 100
+ */
 public class Question226 {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)),
